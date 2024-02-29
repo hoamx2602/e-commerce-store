549bb625a43f9794aa295ccfae49ee3c6f0929d8
hoamx <hoamx.work@gmail.com>
2024-02-29 23:31:39 +0700

    fix footer and productlist too close

--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,11 @@ const HomePage = async () => {
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={products} />
-      </div>
+
     </Container>
   );
 };
