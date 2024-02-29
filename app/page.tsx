f7f5733e7430a63c160910064bda8b5012feaa8b
hoamx <hoamx.work@gmail.com>
2024-02-29 23:05:05 +0700

    show billboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("c490ce42-f958-45ab-a02a-88ad8b2aa3a3");
   return (
-    <div>Hello Store</div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
   );
 };
 
