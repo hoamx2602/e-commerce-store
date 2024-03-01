a4c050b9ad75ab5da85959275d5117cd32d264ce
hoamx <hoa.mx@kiotviet.com>
2024-03-01 08:09:32 +0700

    handle infor product

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
 import Gallery from "@/components/gallery";
+import Info from "@/components/info";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
@@ -26,10 +27,8 @@ const ProductPage: React.FC<ProductPageProsp> = async ({
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
             <Gallery images={product?.images}/>
-            <div>Gallery</div>
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-              {/* Info */}
-              Info
+              <Info data={product}/>
             </div>
           </div>
           <hr className="my-10"/>
