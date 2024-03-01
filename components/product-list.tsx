b0b10840e44d56402a8596eb36168bdf743ea8f8
hoamx <hoa.mx@kiotviet.com>
2024-03-01 12:44:31 +0700

    handle product by category and filter

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,6 +1,6 @@
 import { Product } from "@/types";
 import NoResults from "@/components/ui/no-results";
-import ProductCard from "@/components/ui/product-cart";
+import ProductCard from "@/components/ui/product-card";
 
 interface ProductListProps {
   title: string;
